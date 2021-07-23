10775d2f88139cba72b85cad196ed8cd5c8243f7
papanayo2 <chrispapanagiotou@gmail.com>
2021-07-22 20:40:14 -0400

    lalalaa

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,15 @@
-import { render, screen } from '@testing-library/react';
+mport React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  true
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
